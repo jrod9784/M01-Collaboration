e5ef81cfbe652374b8113c86cfb92331169de65b
jrod9784 <93807631+jrod9784@users.noreply.github.com>
2022-08-24 22:22:51 -0400

    Update CaseStudy.py

--- CaseStudy.py
+++ CaseStudy.py
@@ -1,3 +1,7 @@
+# Jordan Luers
+# Dean's list checker
+# This app will accept student input for last name and first name, then accept input for their gpa. Then tell the student if they made the dean's list, Honor Roll, or nothing at all
+
 studentLastName = input("Enter last name or 'ZZZ' to exit.")
 studentFirstName = ""
 GPA = 0.0
